ca10bf7bdfd6f21b8cfec3f066231da296167ebd
song ji hyeok <porr3444@naver.com>
2017-08-01 22:40:54 +0900

    exercise

---
+++ src/main/java/com/example/demo/LukeskywalkerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukeskywalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukeskywalkerApplication.class, args);
+	}
+}
